e3945499f70bfced3b2ed32ae0f8642dee4cbab1
Devix-old <mohamed.elgharef@gmail.com>
2024-08-21 17:44:38 +0100

    hhh

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- create table, add column with ENUMERATION !
-CREATE TABLE IF NOT EXISTS `users`(
+CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email CHAR(255) NOT NULL UNIQUE,
 	name CHAR(255),
-	country ENUM('US', 'CO', 'IN') NOT NULL DEFAULT 'US'
+	country ENUM('US', 'CO', 'IN') NOT NULL
 );
 
