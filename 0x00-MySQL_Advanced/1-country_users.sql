c9b27e86541f7ddde9e38bf34659f093ed3b924a
Devix-old <mohamed.elgharef@gmail.com>
2024-08-21 17:46:52 +0100

    hhh

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,10 @@
 -- create table, add column with ENUMERATION !
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
+-- If the table already exists, your script should not fail
+-- Your script can be executed on any database
 CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email CHAR(255) NOT NULL UNIQUE,
