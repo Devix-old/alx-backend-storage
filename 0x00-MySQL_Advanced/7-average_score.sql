e16d12c1841c5fbe22294dbd0c07d8a09dcaef67
Devix-old <mohamed.elgharef@gmail.com>
2024-08-23 15:16:12 +0100

    avg(score)

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,15 @@
+--  SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id int
+)
+BEGIN
+  DECLARE average_score FLOAT;
+  SELECT AVG(score) INTO @average_score FROM corrections WHERE corrections.user_id = user_id;
+  UPDATE users
+  SET average_score = @average_score WHERE id = user_id;
+END //
+
+DELIMITER ;
