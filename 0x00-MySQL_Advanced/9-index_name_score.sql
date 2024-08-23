0690ff226dcef8955a5c8bda3c8654bcd1fd7bfa
Devix-old <mohamed.elgharef@gmail.com>
2024-08-23 17:12:50 +0100

    new_Updates

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- Create index named idx_name_firston names talbe with the first letter of each name AND score
 
-CREATE INDEX idx_name_first ON names (name(1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
