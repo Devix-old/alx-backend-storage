6cb21f6254140554c6e7d7064c25d829c79c13c3
Devix-old <mohamed.elgharef@gmail.com>
2024-08-27 05:07:21 +0100

    0x01-NoSQL

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,5 +5,5 @@
 def update_topics(mongo_collection, name, topics):
     """change all topics of a school document based on the name"""
     return mongo_collection.updateMany(
-        {'name': name}, {'$set': {'topics': topics}}
+        {"name": name}, {"$set": {"topics": topics}}
     )
