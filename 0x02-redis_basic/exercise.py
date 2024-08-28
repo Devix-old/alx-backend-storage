3b51cd01b9dbf262f2452ba8cbe7554339c77092
Devix-old <mohamed.elgharef@gmail.com>
2024-08-28 19:36:23 +0100

    0x02-redis_basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import redis
 import uuid
+from typing import Union
 """Redis module"""
 
 
@@ -10,7 +11,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union(str, bytes, int, float)) -> str:
         id = uuid.uuid4()
         self._redis.set(str(id), data)
         return str(id)
