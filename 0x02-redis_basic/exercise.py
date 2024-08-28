3ab1f12833a1b1080dd7cc2cf02a3279374b172a
Devix-old <mohamed.elgharef@gmail.com>
2024-08-28 19:33:04 +0100

    0x02-redis_basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,7 @@ import uuid
 class Cache:
     """Cache class"""
     def __init__(self) -> None:
-        self._redis = redis.Redis(host='localhost', port=6379)
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data) -> str:
